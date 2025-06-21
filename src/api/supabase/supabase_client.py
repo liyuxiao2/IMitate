43e5f56e11e21c5779575a62ac85053737da5b6f
liyuxiao2 <liyuxiao2@gmail.com>
2025-06-21 19:44:52 -0400

    added profile supabase

--- src/api/supabase/supabase_client.py
+++ src/api/supabase/supabase_client.py
@@ -3,7 +3,8 @@ from supabase import create_client
 from dotenv import load_dotenv
 import os
 
-load_dotenv(".env")  # or ".env.backend"
+# Load the .env.local file
+load_dotenv(".env.local")
 
 SUPABASE_URL = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
 SUPABASE_KEY = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
